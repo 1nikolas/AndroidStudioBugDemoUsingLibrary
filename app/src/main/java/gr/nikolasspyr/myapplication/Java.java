fe6c992cadd2e040246ece6594e052bf40c79293
Nikolas Spiridakis <1nikolas@hotmail.gr>
2022-09-14 18:30:27 +0300

    first commit

---
+++ app/src/main/java/gr/nikolasspyr/myapplication/Java.java
@@ -0,0 +1,10 @@
+package gr.nikolasspyr.myapplication;
+
+import androidx.recyclerview.widget.RecyclerView;
+import demo.library.A;
+
+public class Java {
+    void test(){
+        RecyclerView.ItemDecoration test = new A();
+    }
+}
---
+++ app/src/main/java/gr/nikolasspyr/myapplication/Kotlin.kt
@@ -0,0 +1,10 @@
+package gr.nikolasspyr.myapplication
+
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+import demo.library.A
+
+class Kotlin {
+    fun test(){
+        val test: ItemDecoration = A()
+    }
+}
